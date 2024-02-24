10b0d581d3368f94758d5a0e3d9b7f1dcc2d3b04
mansourr157 <mohamed0155015512@yahoo.com>
2024-02-24 21:32:05 +0200

    added operators

---
+++ operators/1_arthymatic_operators.py
@@ -0,0 +1,14 @@
+#############################################
+######### Arthimatic operators ##############
+#############################################
+
+print(5+6)
+print(5-6)
+print(5*6)
+print("======")
+print(10/2)
+print(21/5)         #Division
+print(21//5)        #floor division
+print(11%2)         #Modolus
+print("======")
+print(2**3)         #power
